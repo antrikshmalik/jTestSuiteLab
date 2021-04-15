6665db0a4e5fa2412bf920db7708e773728ee765
antrikshmalik <malikantriksh@gmail.com>
2021-04-15 10:35:00 +0530

    First commit

---
+++ jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,10 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	public int addNums (int N1, int N2) {
+		return N1 + N2;
+	}
+	public String addStrings (String s1, String s2) {
+		return s1 + s2;
+	}
+}
